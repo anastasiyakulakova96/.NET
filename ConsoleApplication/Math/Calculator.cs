01a7360521363f6ec8976eeeed7e7c67366a2c94
AnastasiyaKulakova <kylalova.n@mail.ru>
2016-06-28 16:31:00 +0300

    add existing projects

---
+++ ConsoleApplication/Math/Calculator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit;
+using OpenQA.Selenium;
+using Microsoft.VisualStudio.QualityTools.UnitTestFramework;
+using System.Configuration;
+
+namespace Math
+{
+    public class Calculator 
+    {
+      public  long Sum(long firstNumber,long secondNumber)
+        { 
+            return (firstNumber + secondNumber);
+        }
+
+        public long Subtraction(long firstNumber, long secondNumber)
+        {
+            return (firstNumber - secondNumber);
+        }
+
+        public long Multiplication(long firstNumber, long secondNumber)
+        {
+            return (firstNumber * secondNumber);
+        }
+
+        public double Division(double firstNumber, double secondNumber)
+        {
+            try
+            {
+                return (firstNumber / secondNumber);
+            }
+            catch (ArithmeticException)
+            {
+                return  double.PositiveInfinity;
+            }
+
+        }
+
+    }
+}
---
+++ ConsoleApplication/Test/TestClass.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Math;
+
+namespace Test
+{
+    public class TestClass
+    {
+        [Microsoft.VisualStudio.TestTools.UnitTesting.TestClass]
+        public class MyTestClass
+        {
+            int firstNumber = Int32.Parse(Resource.firstNumber);
+            int secondNumber = Int32.Parse(Resource.secondNumber);
+
+            Calculator calc = new Calculator();
+
+            [TestMethod]
+            public void TestCheckSum()
+            {
+                Assert.AreEqual(((long)firstNumber + (long)secondNumber), calc.Sum(firstNumber, secondNumber));
+            }
+
+            [TestMethod]
+            public void TestCheckSubtraction()
+            {
+                Assert.AreEqual(((long)firstNumber - (long)secondNumber), calc.Subtraction(firstNumber, secondNumber));
+            }
+
+            [TestMethod]
+            public void TestCheckMultiplication()
+            {
+                Assert.AreEqual(((long)firstNumber * (long)secondNumber), calc.Multiplication(firstNumber, secondNumber));
+            }
+
+            [TestMethod]
+            public void TestCheckDivision()
+            {
+                Assert.AreEqual(((double)firstNumber / (double)secondNumber), calc.Division(firstNumber, secondNumber));
+            }
+        }
+    }
+}
